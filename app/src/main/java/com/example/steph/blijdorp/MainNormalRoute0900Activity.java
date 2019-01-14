9c07901d38123c2247c45e3c07323b0c870f367f
SGordijn <stephangordijn@gmail.com>
2019-01-14 10:50:52 +0100

    Finished Normal Route 0900 Map 14-1

--- app/src/main/java/com/example/steph/blijdorp/MainNormalRoute0900Activity.java
+++ app/src/main/java/com/example/steph/blijdorp/MainNormalRoute0900Activity.java
@@ -194,6 +194,170 @@ LocationListener {
         LatLng routeMain4 = new LatLng(51.928405, 4.451422);
         mMap.addMarker(new MarkerOptions().position(routeMain4).title("Ga het Taman Indah in."));
 
+        Polyline line8 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.928405, 4.451422), new LatLng(51.928252, 4.452160))
+                .width(5)
+                .color(Color.RED));
+
+        LatLng routeMain5 = new LatLng(51.928252, 4.452160);
+        mMap.addMarker(new MarkerOptions().position(routeMain5).title("Ga het Taman Indah uit."));
+
+        Polyline line9 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.928252, 4.452160), new LatLng(51.927935, 4.454023))
+                .width(5)
+                .color(Color.RED));
+
+        LatLng routeMain6 = new LatLng(51.927935, 4.454023);
+        mMap.addMarker(new MarkerOptions().position(routeMain6).title("Ga naar Toko Tjitjak. Ga dan richting het zuiden."));
+
+        Polyline line10 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.927935, 4.454023), new LatLng(51.927320, 4.453387))
+                .width(5)
+                .color(Color.RED));
+
+        LatLng routeMain7 = new LatLng(51.927320, 4.453387);
+        mMap.addMarker(new MarkerOptions().position(routeMain7).title("Ga rechts naar de apen toe."));
+
+        Polyline line11 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.927320, 4.453387), new LatLng(51.927615, 4.452266))
+                .width(5)
+                .color(Color.RED));
+
+        Polyline line12 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.927615, 4.452266), new LatLng(51.927052, 4.452889))
+                .width(5)
+                .color(Color.RED));
+
+        LatLng routeMain8 = new LatLng(51.927052, 4.452889);
+        mMap.addMarker(new MarkerOptions().position(routeMain8).title("Ga nu richting het Rivièrahal."));
+
+        Polyline line13 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.927052, 4.452889), new LatLng(51.926839, 4.452197))
+                .width(5)
+                .color(Color.RED));
+
+        Polyline line14 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.926839, 4.452197), new LatLng(51.926578, 4.451923))
+                .width(5)
+                .color(Color.RED));
+
+        LatLng routeMain9 = new LatLng(51.926578, 4.451923);
+        mMap.addMarker(new MarkerOptions().position(routeMain9).title("Ga lekker lunchen. (Optioneel)"));
+
+        Polyline line15 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.926578, 4.451923), new LatLng(51.926215, 4.451644))
+                .width(5)
+                .color(Color.RED));
+
+        Polyline line16 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.926215, 4.451644), new LatLng(51.925976, 4.451065))
+                .width(5)
+                .color(Color.RED));
+
+        LatLng routeMain10 = new LatLng(51.925976, 4.451065);
+        mMap.addMarker(new MarkerOptions().position(routeMain10).title("Bekijk de neushoorns."));
+
+        Polyline line17 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.925976, 4.451065), new LatLng(51.925662, 4.451177))
+                .width(5)
+                .color(Color.RED));
+
+        LatLng routeMain11 = new LatLng(51.925662, 4.451177);
+        mMap.addMarker(new MarkerOptions().position(routeMain11).title("Vergeet ook niet de dwergnijlpaarden."));
+
+        Polyline line18 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.925662, 4.451177), new LatLng(51.925460, 4.450932))
+                .width(5)
+                .color(Color.RED));
+
+        LatLng routeMain12 = new LatLng(51.925460, 4.450932);
+        mMap.addMarker(new MarkerOptions().position(routeMain12).title("Bekijk de okapi's en de krokodillen."));
+
+        Polyline line19 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.925460, 4.450932), new LatLng(51.925761, 4.450588))
+                .width(5)
+                .color(Color.RED));
+
+        Polyline line20 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.925761, 4.450588), new LatLng(51.925609, 4.450111))
+                .width(5)
+                .color(Color.RED));
+
+        LatLng routeMain13 = new LatLng(51.925609, 4.450111);
+        mMap.addMarker(new MarkerOptions().position(routeMain13).title("Bekijk de giraffen."));
+
+        Polyline line21 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.925609, 4.450111), new LatLng(51.925754, 4.449661))
+                .width(5)
+                .color(Color.RED));
+
+        Polyline line22 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.925754, 4.449661), new LatLng(51.926254, 4.449538))
+                .width(5)
+                .color(Color.RED));
+
+        Polyline line23 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.926254, 4.449538), new LatLng(51.926617, 4.449824))
+                .width(5)
+                .color(Color.RED));
+
+        LatLng routeMain14 = new LatLng(51.926617, 4.449824);
+        mMap.addMarker(new MarkerOptions().position(routeMain14).title("Ga nu richting de tijgers. Je bent er bijna!"));
+
+        Polyline line24 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.926617, 4.449824), new LatLng(51.927579, 4.449496))
+                .width(5)
+                .color(Color.RED));
+
+        Polyline line25 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.927579, 4.449496), new LatLng(51.927602, 4.449692))
+                .width(5)
+                .color(Color.RED));
+
+        Polyline line26 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.927602, 4.449692), new LatLng(51.926999, 4.450771))
+                .width(5)
+                .color(Color.RED));
+
+        Polyline line27 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.926999, 4.450771), new LatLng(51.927192, 4.451328))
+                .width(5)
+                .color(Color.RED));
+
+        LatLng routeMain15 = new LatLng(51.927192, 4.451328);
+        mMap.addMarker(new MarkerOptions().position(routeMain15).title("Bekijk de tijgers. Ga hierna naar de olifanten."));
+
+        Polyline line28 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.927192, 4.451328), new LatLng(51.927326, 4.451759))
+                .width(5)
+                .color(Color.RED));
+
+        Polyline line29 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.927326, 4.451759), new LatLng(51.927481, 4.451839))
+                .width(5)
+                .color(Color.RED));
+
+        Polyline line30 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.927481, 4.451839), new LatLng(51.927577, 4.451029))
+                .width(5)
+                .color(Color.RED));
+
+        LatLng routeMain16 = new LatLng(51.927577, 4.451029);
+        mMap.addMarker(new MarkerOptions().position(routeMain16).title("Bekijk de olifanten en rond de route af."));
+
+        Polyline line31 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.927577, 4.451029), new LatLng(51.927791, 4.450472))
+                .width(5)
+                .color(Color.RED));
+
+        Polyline line32 = mMap.addPolyline(new PolylineOptions()
+                .add(new LatLng(51.927791, 4.450472), new LatLng(51.928225, 4.450200))
+                .width(5)
+                .color(Color.RED));
+
+        LatLng routeMain17 = new LatLng(51.928225, 4.450200);
+        mMap.addMarker(new MarkerOptions().position(routeMain17).title("Einde route. Vergeet niet souveniers te kopen."));
+
         // Add a marker in Sydney and move the camera
 //        LatLng sydney = new LatLng(-34, 151);
 //        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
